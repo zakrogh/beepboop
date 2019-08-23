cff13353b80f644318eeb8a76978d2eee93e5c15
Zach Krogh <zakrogh@gmail.com>
2019-08-23 12:18:07 -0700

    allow users to input a name, restrict input to positive integers, add some colors to various html elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-var beepBoop = function(num){
+//back end
+
+var beepBoop = function(num, name){
+  var userName = name;
+  if(!name){
+    userName = "Dave";
+  }
   var outputA = [];
   for(var i = 0; i <= num; i++){
     if(i.toString().includes("3")){
-      outputA.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      outputA.push("I'm sorry, " + userName + ". I'm afraid I can't do that.");
     }else if (i.toString().includes("2")) {
       outputA.push("Boop!");
     }else if (i.toString().includes("1")) {
@@ -15,12 +21,23 @@ var beepBoop = function(num){
   return outputA;
 }
 
+//front end
+var displayOutput = function(arr){
+  $(".output").text("");
+  $(".output").append(arr.join(" "));
+  $(".output").hide();
+  $(".output").fadeIn();
+  $(".userInput").select();
+}
+
 $(document).ready(function(){
   $(".mainform").submit(function(event){
+    $(".inputGroup").removeClass("has-error");
     var userInput = parseInt($(".userInput").val());
-    var output = beepBoop(userInput);
-    $(".output").append(output);
-    $(".output").fadeToggle();
+    var userName = $(".userName").val();
+    var output = beepBoop(userInput, userName);
+    displayOutput(output);
+
     event.preventDefault();
   });
 });
